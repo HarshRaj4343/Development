ba9e6110795c3d87d4c9bae4ecb052468a9de338
Harsh Raj <keshriayush321@gmail.com>
2026-08-25 19:12:25 +0530

    js2

---
+++ JavaScript-Babbar/loops.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i+1);
+}
+
+let j = 0;
+while (j < 5) {
+    console.log(j+1);
+    j++;
+}
+
+let k = 0;
+
+do {
+    console.log(k+1);
+    k++;
+} while (k < 5);
+
+
+
