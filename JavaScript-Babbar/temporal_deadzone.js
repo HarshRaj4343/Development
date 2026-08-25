f4bf05275766ec50abdde5b475e2605cdb0dce14
Harsh Raj <keshriayush321@gmail.com>
2026-08-25 23:54:37 +0530

    JavaScript

---
+++ JavaScript-Babbar/temporal_deadzone.js
@@ -0,0 +1,7 @@
+console.log(marks); // throws an error 
+console.log("harsh") 
+let marks = 100;
+
+// line 1 to 2 are temporal deadzone as marks is declared using let and it is not initialized before line 1. So, it throws an error.
+
+
