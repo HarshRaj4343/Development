9b091bbecd3c1b2e90d85b5ab8ad578f08ee0bf1
Harsh Raj <keshriayush321@gmail.com>
2026-08-26 11:36:04 +0530

    DOM HTML Manipulation

---
+++ JavaScript-Babbar/DOM/js-dom-manipulation.js
@@ -0,0 +1,25 @@
+// window object is the global object in the browser environment. It represents the window in which the script is running. It is top of the root element in the hierarchy of the DOM tree. It is created by the browser.
+
+/*
+
+Document Object Model (DOM):-
+What we do here that we just make an object of the HTML document and we can access all the elements of the HTML document using this object.
+
+The tree:-
+Characters --> HTML Tags --> Tokens (Using Tokenizer) --> Nodes (Using Parser) --> DOM (Using Tree Builder - makes a tree structure of the nodes)
+
+All things like BOM(Browser Object Model), DOM, and JavaScript are all part of the window object.
+
+
+(Browser Object Model) BOM:-
+The Browser Object Model (BOM) is a set of objects provided by the browser that allows JavaScript to interact with the browser itself. It provides access to the browser's features and functionality, such as the window, navigator, screen, history, and location objects. The BOM allows developers to manipulate the browser environment and create dynamic web applications. "Except the HTML document, all the other things are part of the BOM."
+
+*/
+
+
+
+
+
+
+
+
